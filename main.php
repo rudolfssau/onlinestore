f5196920dd753138e6aa435d35dcca75076385c2
rudolfssau <rudolfssau@gmail.com>
2022-05-30 12:53:11 +0300

    Making a navbar

--- main.php
+++ main.php
@@ -3,11 +3,22 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="stylesheet" href="main.css">
 </head>
 <body>
 <header>
-    <nav></nav>
+    <nav class="navbar">
+        <a href="#" class="navbar-name">Quickify</a>
+        <ul class="navbar-items">
+            <li><a href="#">E-Store</a></li>
+            <li><a href="#">Best Deals</a></li>
+            <li><a href="#">Services</a></li>
+            <li><a href="#">Our Stores</a></li>
+            <li><a href="#">Contact Us</a></li>
+        </ul>
+    </nav>
 </header>
+
 <footer>
 
 </footer>
