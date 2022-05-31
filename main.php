75089feaafe1db477e7f2913bc6c3d752ce498b5
rudolfssau <rudolfssau@gmail.com>
2022-05-31 14:07:18 +0300

    Adds header styling

--- main.php
+++ main.php
@@ -3,6 +3,10 @@
 <head>
     <meta charset="UTF-8">
     <title>Title</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro&display=swap" rel="stylesheet">
+    <!--    https://fonts.google.com/specimen/Source+Sans+Pro    Link to font-->
     <link rel="stylesheet" href="main.css">
 </head>
 <body>
@@ -10,12 +14,17 @@
     <nav class="navbar">
         <a href="#" class="navbar-name">Quickify</a>
         <ul class="navbar-items">
-                <li><a href="#">E-Store</a></li>
-                <li><a href="#">Best Deals</a></li>
-                <li><a href="#">Services</a></li>
-                <li><a href="#">Our Stores</a></li>
-                <li><a href="#">Contact Us</a></li>
+                <li><a href="#" class="navbar-item">E-Store</a></li>
+                <li><a href="#" class="navbar-item">Best Deals</a></li>
+                <li><a href="#" class="navbar-item">Services</a></li>
+                <li><a href="#" class="navbar-item">Our Stores</a></li>
+                <li><a href="#" class="navbar-item">Contact Us</a></li>
         </ul>
+        <div class="dropdown">
+            <span class="row"></span>
+            <span class="row"></span>
+            <span class="row"></span>
+        </div>
     </nav>
 </header>
 
