63cf6215c9638498071dd98978f6593dc1cce824
rudolfssau <rudolfssau@gmail.com>
2022-05-31 11:44:39 +0300

    Navbar styling done

--- main.php
+++ main.php
@@ -10,11 +10,11 @@
     <nav class="navbar">
         <a href="#" class="navbar-name">Quickify</a>
         <ul class="navbar-items">
-            <li><a href="#">E-Store</a></li>
-            <li><a href="#">Best Deals</a></li>
-            <li><a href="#">Services</a></li>
-            <li><a href="#">Our Stores</a></li>
-            <li><a href="#">Contact Us</a></li>
+                <li><a href="#">E-Store</a></li>
+                <li><a href="#">Best Deals</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Our Stores</a></li>
+                <li><a href="#">Contact Us</a></li>
         </ul>
     </nav>
 </header>
