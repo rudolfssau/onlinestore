145f1c6a600e14b24c4ff6dee40e496bff86398e
rudolfssau <rudolfssau@gmail.com>
2022-06-17 17:40:40 +0300

    Header positioning fixed

--- main.php
+++ main.php
@@ -11,7 +11,10 @@
 </head>
 <body>
 <header>
-    <nav class="navbar">
+    <div id="freeshipping">
+        <h3>FREE SHIPPING over $50*</h3>
+    </div>
+    <nav id="navbar">
         <a href="main.php" class="navbar-name">Quickify</a>
         <ul class="navbar-items">
                 <li><a href="estore.php" class="navbar-item">E-Store</a></li>
@@ -29,135 +32,148 @@
     <script src="dropdown.js"></script>
 </header>
     <div id="main-info">
-        <div id="main-news">
-            <h2>Quick Tip Of The Day</h2>
-            <div>
-                <h2>The main cause for fruit to go bad?</h2>
-                <h3>Microscopic bacteria cause food to spoil. These tiny organisms, called spoilage bacteria, consume unprotected foods and produce waste products. As long as nutrition and water are present, bacteria will multiply, sometimes rapidly. Bacterial waste is the cause of the foul smell and rotten appearance of spoiled food.</h3>
-                <h3>Prevent this by storing your food in dry places and not leaving it out long.</h3>
-                <div><a href="#">Continue reading</a></div>
-            </div>
-        </div>
-        <div id="main-new">
-            <h2>Delivered Today</h2>
-            <div>
-                <section id="product-container">
-                    <div id="product-newnow">
-                    </div>
-                </section>
-            </div>
-        </div>
-        <div id="main-recipes">
-            <h2>Our Recommended Recipes</h2>
-            <section id="recipes01">
-                <div id="recipe0-container">
-                    <a href="#" class="recipe0">
-                        <section class="bananabread">
-                            <img src="recipes/bananabread.jpg" alt="#">
-                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">12 May 2022</h4><span class="spacer"></span></div>
-                            <h3>The Best Banana Bread Ever!</h3>
-                        </section>
-                    </a>
-                </div>
-                <div id="recipe1-container">
-                    <a href="#" class="recipe1">
-                        <section class="chickenpie">
-                            <img src="recipes/chickenpie.jpg" alt="#">
-                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">3 June 2022</h4><span class="spacer"></span></div>
-                            <h3>Our Favorite, The Chicken Potpie.</h3>
-                        </section>
-                    </a>
-                </div>
-                <div id="recipe4-container">
-                    <a href="#" class="recipe4">
-                        <section class="bread">
-                            <img src="recipes/bread.jpg" alt="#">
-                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">8 July 2022</h4><span class="spacer"></span></div>
-                            <h3>Healthy, Homemade Bread</h3>
-                        </section>
-                    </a>
-                </div>
-            </section>
-            <section id="recipes23">
-                <div id="recipe2-container">
-                    <a href="#" class="recipe2">
-                        <section class="applepie">
-                            <img src="recipes/applepie.jpg" alt="#">
-                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">25 August 2022</h4><span class="spacer"></span></div>
-                            <h3>Tastiest Apple Pie EVER, Period.</h3>
-                        </section>
-                    </a>
-                </div>
-                <div id="recipe3-container">
-                    <a href="#" class="recipe3">
-                        <section class="lasagna">
-                            <img src="recipes/lasagna.jpg" alt="#">
-                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">8 July 2022</h4><span class="spacer"></span></div>
-                            <h3>Traditional Lasagna</h3>
-                        </section>
-                    </a>
-                </div>
-                <div id="recipe5-container">
-                    <a href="#" class="recipe5">
-                        <section class="cake">
-                            <img src="recipes/cake.jpg" alt="#">
-                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">8 July 2022</h4><span class="spacer"></span></div>
-                            <h3>Moist Chocolate Cake</h3>
-                        </section>
-                    </a>
-                </div>
-            </section>
-        </div>
-        <div id="main-delivery">
-            <h2>How We Simplify Food Delivery</h2>
-            <div>
-                <h1>We also offer food delivery right to your door!</h1>
-                <h2>During the covid lockdown food delivery has become a more and more popular way to receive food. We have made this process really easy and accessible, with only a few steps you can get your food delivered right to your doorstep.</h2>
-                <h1>How </h1>
-                <img src="Pics/delivery.jpg" alt="#">
-            </div>
-        </div>
+        <img src="https://template66566.motopreview.com/mt-demo/66500/66566/mt-content/uploads/2018/07/mt-1533_home_slider-1.jpg" alt="#">
+        <img src="https://template66566.motopreview.com/mt-demo/66500/66566/mt-content/uploads/2018/07/mt-1533_home_slider-1.jpg" alt="#">
+        <img src="https://template66566.motopreview.com/mt-demo/66500/66566/mt-content/uploads/2018/07/mt-1533_home_slider-1.jpg" alt="#">
+        <img src="https://template66566.motopreview.com/mt-demo/66500/66566/mt-content/uploads/2018/07/mt-1533_home_slider-1.jpg" alt="#">
+        <img src="https://template66566.motopreview.com/mt-demo/66500/66566/mt-content/uploads/2018/07/mt-1533_home_slider-1.jpg" alt="#">
+        <img src="https://template66566.motopreview.com/mt-demo/66500/66566/mt-content/uploads/2018/07/mt-1533_home_slider-1.jpg" alt="#">
+        <img src="https://template66566.motopreview.com/mt-demo/66500/66566/mt-content/uploads/2018/07/mt-1533_home_slider-1.jpg" alt="#">
+        <img src="https://template66566.motopreview.com/mt-demo/66500/66566/mt-content/uploads/2018/07/mt-1533_home_slider-1.jpg" alt="#">
+        <img src="https://template66566.motopreview.com/mt-demo/66500/66566/mt-content/uploads/2018/07/mt-1533_home_slider-1.jpg" alt="#">
     </div>
-<footer>
-    <div id="footer-top">
-        <section>
-            <h3>Accepted payment methods</h3>
-            <div>
-                <img src="" alt="#">
-                <img src="" alt="#">
-                <img src="" alt="#">
-            </div>
-        </section>
-        <section>
-            <h3>Our food delivery partner</h3>
-            <img src="" alt="#">
-        </section>
-    </div>
-    <div id="footer-bottom">
-        <section>
-            <h3>More information</h3>
-            <ul>
-                <li><a href="#">FAQ</a></li>
-                <li><a href="#">Apply for a job</a></li>
-                <li><a href="#">Terms & Conditions</a></li>
-                <li><a href="#">Supplier info</a></li>
-            </ul>
-        </section>
-        <section>
-            <h3>Social Media</h3>
-            <div>
-                <img src="" alt="#">
-                <img src="" alt="#">
-                <img src="" alt="#">
-            </div>
-        </section>
-        <section>
-            <h3>Email subscription</h3>
-            <h4>Become a subscriber and get alerts on the newest deals and offer specifically for you!</h4>
-<!--            Make a subscription box HERE-->
-        </section>
-    </div>
+<!--        <div id="main-new">-->
+<!--            <h2>Delivered Today</h2>-->
+<!--            <div>-->
+<!--                <section id="product-container">-->
+<!--                    <div id="product-newnow">-->
+<!--                    </div>-->
+<!--                </section>-->
+<!--            </div>-->
+<!--        </div>-->
+<!--        <div id="main-recipes">-->
+<!--            <h2>Our Recommended Recipes</h2>-->
+<!--            <section id="recipes01">-->
+<!--                <div id="recipe0-container">-->
+<!--                    <a href="#" class="recipe0">-->
+<!--                        <section class="bananabread">-->
+<!--                            <img src="recipes/bananabread.jpg" alt="#">-->
+<!--                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">12 May 2022</h4><span class="spacer"></span></div>-->
+<!--                            <h3>The Best Banana Bread Ever!</h3>-->
+<!--                        </section>-->
+<!--                    </a>-->
+<!--                </div>-->
+<!--                <div id="recipe1-container">-->
+<!--                    <a href="#" class="recipe1">-->
+<!--                        <section class="chickenpie">-->
+<!--                            <img src="recipes/chickenpie.jpg" alt="#">-->
+<!--                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">3 June 2022</h4><span class="spacer"></span></div>-->
+<!--                            <h3>Our Favorite, The Chicken Potpie.</h3>-->
+<!--                        </section>-->
+<!--                    </a>-->
+<!--                </div>-->
+<!--                <div id="recipe4-container">-->
+<!--                    <a href="#" class="recipe4">-->
+<!--                        <section class="bread">-->
+<!--                            <img src="recipes/bread.jpg" alt="#">-->
+<!--                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">8 July 2022</h4><span class="spacer"></span></div>-->
+<!--                            <h3>Healthy, Homemade Bread</h3>-->
+<!--                        </section>-->
+<!--                    </a>-->
+<!--                </div>-->
+<!--            </section>-->
+<!--            <section id="recipes23">-->
+<!--                <div id="recipe2-container">-->
+<!--                    <a href="#" class="recipe2">-->
+<!--                        <section class="applepie">-->
+<!--                            <img src="recipes/applepie.jpg" alt="#">-->
+<!--                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">25 August 2022</h4><span class="spacer"></span></div>-->
+<!--                            <h3>Tastiest Apple Pie EVER, Period.</h3>-->
+<!--                        </section>-->
+<!--                    </a>-->
+<!--                </div>-->
+<!--                <div id="recipe3-container">-->
+<!--                    <a href="#" class="recipe3">-->
+<!--                        <section class="lasagna">-->
+<!--                            <img src="recipes/lasagna.jpg" alt="#">-->
+<!--                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">8 July 2022</h4><span class="spacer"></span></div>-->
+<!--                            <h3>Traditional Lasagna</h3>-->
+<!--                        </section>-->
+<!--                    </a>-->
+<!--                </div>-->
+<!--                <div id="recipe5-container">-->
+<!--                    <a href="#" class="recipe5">-->
+<!--                        <section class="cake">-->
+<!--                            <img src="recipes/cake.jpg" alt="#">-->
+<!--                            <div class="spacer-container"><span class="spacer"></span><h4 class="recipes-date">8 July 2022</h4><span class="spacer"></span></div>-->
+<!--                            <h3>Moist Chocolate Cake</h3>-->
+<!--                        </section>-->
+<!--                    </a>-->
+<!--                </div>-->
+<!--            </section>-->
+<!--        </div>-->
+<!--        <div id="main-delivery">-->
+<!--            <h2>How We Simplify Food Delivery</h2>-->
+<!--            <div>-->
+<!--                <h1>We also offer food delivery right to your door!</h1>-->
+<!--                <h2>During the covid lockdown food delivery has become a more and more popular way to receive food. We have made this process really easy and accessible, with only a few steps you can get your food delivered right to your doorstep.</h2>-->
+<!--                <h1>What delivery options we offer?</h1>-->
+<!--                <h2>When checking out, you can select how would you like the delivery to happen, we offer many ways of delivering.</h2>-->
+<!--                <section>-->
+<!--                    <h1>Home Delivery</h1>-->
+<!---->
+<!--                </section>-->
+<!--                <section>-->
+<!--                    <h1>In-Store Pickup</h1>-->
+<!--                </section>-->
+<!--                <section>-->
+<!--                    <h1>Remote Collection Points</h1>-->
+<!--                </section>-->
+<!--                <img src="Pics/delivery.jpg" alt="#">-->
+<!--            </div>-->
+<!--        </div>-->
+<!--    </div>-->
+<!--<footer>-->
+<!--    <div id="footer-top">-->
+<!--        <section>-->
+<!--            <h3>Accepted payment methods</h3>-->
+<!--            <div>-->
+<!--                <img src="" alt="#">-->
+<!--                <img src="" alt="#">-->
+<!--                <img src="" alt="#">-->
+<!--            </div>-->
+<!--        </section>-->
+<!--        <section>-->
+<!--            <h3>Our food delivery partner</h3>-->
+<!--            <img src="" alt="#">-->
+<!--        </section>-->
+<!--    </div>-->
+<!--    <div id="footer-bottom">-->
+<!--        <section>-->
+<!--            <h3>More information</h3>-->
+<!--            <ul>-->
+<!--                <li><a href="#">FAQ</a></li>-->
+<!--                <li><a href="#">Apply for a job</a></li>-->
+<!--                <li><a href="#">Terms & Conditions</a></li>-->
+<!--                <li><a href="#">Supplier info</a></li>-->
+<!--            </ul>-->
+<!--        </section>-->
+<!--        <section>-->
+<!--            <h3>Social Media</h3>-->
+<!--            <div>-->
+<!--                <img src="" alt="#">-->
+<!--                <img src="" alt="#">-->
+<!--                <img src="" alt="#">-->
+<!--            </div>-->
+<!--        </section>-->
+<!--        <section>-->
+<!--            <h3>Email subscription</h3>-->
+<!--            <h4>Become a subscriber and get alerts on the newest deals and offer specifically for you!</h4>-->
+<!--           Make a subscription box HERE-->
+<!--        </section>-->
+<!--    </div>-->
     <script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+    <script src="styling.js"></script>
     <script src="json.js"></script>
 </footer>
 </body>
---
+++ styling.js
@@ -0,0 +1,11 @@
+$(window).bind('scroll', function() {
+    if ($(this).scrollTop() > 40) {
+        // $('#freeshipping').fadeOut(50);
+        $('#navbar').css({position:'fixed', top:'0'});
+        $('#main-info').css({'margin-top': '61px'});
+    }
+    else {
+        $('#navbar').css({position:'unset'});
+        $('#main-info').css({'margin-top': 'unset'});
+    }
+});
